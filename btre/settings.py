22ba40b2d41ac66b38a8a647d40da9d991bde223
tanveenkaur <tanveenahujaa@gmail.com>
2020-07-25 18:52:54 +0530

    changes added

--- btre/settings.py
+++ btre/settings.py
@@ -143,12 +143,6 @@ MESSAGE_TAGS = {
 
 }
 
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER ='tanveenahujaa@gmail.com'
-EMAIL_HOST_PASSWORD='abhijeet13@'
-EMAIL_USE_TLS=True
-
 
 try:
     from .local_settings import *
